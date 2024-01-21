dd4a35a8fa747d98620f2c2d51a0f2f019e01fc5
Ali <alawi_amer@icloud.com>
2024-01-21 15:18:27 +0300

    Initial Commit

---
+++ atilimSch/atilimSchApp.swift
@@ -0,0 +1,17 @@
+//
+//  atilimSchApp.swift
+//  atilimSch
+//
+//  Created by BLG-BC-018 on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct atilimSchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
